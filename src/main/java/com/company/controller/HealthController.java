d251661b7826b61a2e5620858f2fe4d988baea31
namrahov <amrahovnurlan@gmail.com>
2023-02-23 20:36:46 +0400

    4

---
+++ src/main/java/com/company/controller/HealthController.java
@@ -0,0 +1,19 @@
+package com.company.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<Void> health() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/readiness")
+    public ResponseEntity<Void> readiness() {
+        return ResponseEntity.ok().build();
+    }
+}
